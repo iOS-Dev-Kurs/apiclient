8e92f555cf47f446a672060e38a78aaea8152a8f
lucasmllr <lucasmoeller@me.com>
2016-09-05 16:34:06 +0200

    added all

--- APIClient/CapitalViewController.swift
+++ APIClient/CapitalViewController.swift
@@ -12,35 +12,95 @@ import Freddy
 import Moya
 import Alamofire
 
-class CapitalViewController: UIViewController {
+class CountryViewController: UIViewController {
     
     var provider: MoyaProvider<countryAPI>!
+    
+    var countryToDisplay: Country?
+    
+    @IBOutlet var countryLabel: UILabel!
+    @IBOutlet var CapitalLabel: UILabel!
+    @IBOutlet var populationLabel: UILabel!
 
-    func getCapitalOf(Country: String) -> (Capital: String?, error: ErrorType?){
-        var returnCapital: String? = nil
-        var returnError: ErrorType? = nil
+    @IBOutlet var showCountryLabel: UILabel!
+    @IBOutlet var showCapitalLabel: UILabel!
+    @IBOutlet var showPopulationLabel: UILabel!
+    
+    @IBOutlet var searchField: UITextField!
+    
+    @IBAction func searchButtonPressed(sender: AnyObject) {
+        if let countryToFind = searchField.text {
+        findCountry(countryToFind)
+    } else {
+        searchField.text = "please enter a country"
+        }
+    }
+    /*
+    func getCapitalOf(Country: String) {
         provider.request(.fullName(name: Country)) {result in
             switch result {
             case let .Success(moyaResponse):
                 let data = moyaResponse.data
-                guard let json = try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) else {
+                var json:AnyObject?
+                
+                do
+                {
+                    json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions())
+                    print(json)
+                } catch {
+                    print(error)
+                }
+                
+                guard let jArray = json as? [NSMutableDictionary]
+                    else {
+                        print("format error")
+                        break
+                    }
+                
+                guard let capital = jArray[0]["capital"] as? String else {
+                    print("capital not a String")
                     break
                 }
-                guard let jsonArray = json as? [String: AnyObject] else {
-                    break
-                }
-                guard let capital = jsonArray["capital"] as? String else {
-                    break
-                }
-                returnCapital = capital
+                self.capitalLabel.text = capital
+                
             case let .Failure(error):
-                print("Error")
+                print(error)
             }
         }
-        return (returnCapital, returnError)
+    }
+    */
+    
+    func findCountry(CountryToFind: String) {
+        provider.request(.fullName(name: CountryToFind)) {result in
+            switch result {
+            case let .Success(moyaResponse):
+                let data = moyaResponse.data
+                do {
+                    let json = try JSON(data: data)
+                    let someCountry = try json.array().map(Country.init)[0]
+                    self.countryToDisplay = someCountry
+                    self.displayCountry(self.countryToDisplay)
+                }
+                catch {
+                    print(error)
+                }
+                
+            case let .Failure(error):
+                print(error)
+            }
+        }
     }
     
-    
+    func displayCountry(countryToDisplay: Country?) {
+        if countryToDisplay != nil {
+            showCountryLabel.text = countryToDisplay!.name
+            showCapitalLabel.text = countryToDisplay!.capital
+            showPopulationLabel.text = String(countryToDisplay!.population)
+        } else {
+            searchField.text = "no country to display"
+        }
+    }
+
 
 }
 
--- APIClient/country.swift
+++ APIClient/country.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import Freddy
+import UIKit
+
+public struct Country {
+    
+    let name: String
+    let languages: [JSON]
+    let population: Int
+    let capital: String
+}
+
+extension Country: JSONDecodable {
+    public init(json: JSON) throws {
+        name = try json.string("name")
+        languages = try json.array("languages")
+        population = try json.int("population")
+        capital = try json.string("capital")
+    }
+}
